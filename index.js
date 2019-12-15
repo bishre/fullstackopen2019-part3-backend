d9098555014388176439b04772b9876845c23516
bishre <bibhorshrestha@gmail.com>
2019-12-15 17:04:26 +0200

    test2

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const persons = [
-  {name: 'bibhor', number: 34567890987}
-]
-
 const url =
   `mongodb+srv://bibhor:bibhor34@moverz-s7atf.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
@@ -20,62 +16,10 @@ const contactSchema = new mongoose.Schema({
 })
 
 const Contact = mongoose.model('Contact', contactSchema)
-morgan.token('type', (req, res) => req.body)
+
 app.use(bodyParser.json())
     .use(morgan('tiny'))
     .use(cors())
-    .use(express.static('build'))
-
-app.get("/api/persons", (req, res) => {
-    Contact.find({}).then(persons => {
-        res.json(persons)
-    })
-})
-
-app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    if(person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
-})
-
-app.post('/api/persons', (req, res) => {
-    const body = req.body
-    if(!body.name || !body.number) {
-        res.status(400).json({
-            error: 'missing contact details'
-        })
-    } else if(persons.some(person => person.name === body.name)) {
-        res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random()*1000000000)
-    }
-    persons = persons.concat(person)
-
-    res.json(person)
-})
-
-app.get("/info", (req, res) => {
-    res.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>
-    `)
-})
 
 app.get("/", (req, res) => {
   res.send(`
